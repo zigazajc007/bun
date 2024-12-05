295d5ab3052c9ffce90bce7b5c120720f642b8b9
pfg <pfg@pfg.pw>
2024-12-04 19:27:18 -0800

    fix ci?

--- test/external/external_helper.ts
+++ test/external/external_helper.ts
@@ -1,35 +1,15 @@
-import { $ } from "bun";
 import { existsSync } from "fs";
-import { expect } from "bun:test";
-
-function syncExecPromise<T>(v: () => Promise<T>): T {
-  let err = null;
-  let res: T | null = null;
-  let success = false;
-  expect(
-    (async (): Promise<number> => {
-      try {
-        res = await v();
-      } catch (e) {
-        err = e;
-      }
-      success = true;
-      return 0;
-    })(),
-  ).resolves.toBe(0);
-  if (!success) throw new Error("promise did not sync exec");
-  if (err != null) throw err;
-  return res as T;
-}
-
-// ./vendor/zig/zig.exe fetch
 
 export function fetchZip(url: string, hash: string): string {
-  return syncExecPromise(() => fetchZipAsync(url, hash));
-}
-async function fetchZipAsync(url: string, hash: string): Promise<string> {
-  const zig_exe = "./vendor/zig/zig.exe";
-  const env = await $`${zig_exe} env`.json();
+  const zig_exe = existsSync("./vendor/zig/zig.exe") ? "./vendor/zig/zig.exe" : "zig";
+  const env = JSON.parse(
+    Bun.spawnSync({
+      cmd: [zig_exe, "env"],
+      stdin: "inherit",
+      stdout: "pipe",
+      stderr: "inherit",
+    }).stdout.toString(),
+  );
   const pkg_dir = env.global_cache_dir + "/p";
   const this_pkg_dir = pkg_dir + "/" + hash;
   if (existsSync(this_pkg_dir)) {
@@ -38,7 +18,11 @@ async function fetchZipAsync(url: string, hash: string): Promise<string> {
   }
   // failure; fetch
   console.log("… downloading package: " + url);
-  await $`${zig_exe} fetch ${url}`;
+  const fetchres = Bun.spawnSync({
+    cmd: [zig_exe, "fetch", url],
+    stdio: ["inherit", "inherit", "inherit"],
+  });
+  if (fetchres.exitCode !== 0) throw new Error("bad exit code");
   console.log("→ done!");
   if (existsSync(this_pkg_dir)) {
     // success
